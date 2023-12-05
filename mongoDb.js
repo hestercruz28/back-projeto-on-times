dd98f58cdab9a2b432f68f094ff8bcd2ee375f41
hestercruzz28 <hestercruzcruz@gmail.com>
2023-12-04 22:31:01 -0300

    av2

--- mongoDb.js
+++ mongoDb.js
@@ -1,6 +1,11 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017'; 
+const username = 'hestercruz';
+const password = 'eanjdepP3snCSJcI'; // Substitua pela sua senha
+const dbName = 'users'; // Substitua pelo nome do seu banco de dados
+
+const url = `mongodb+srv://${username}:${password}@cluster0.ehhmsiv.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
 
 async function connectToMongoDB() {
@@ -12,4 +17,14 @@ async function connectToMongoDB() {
   }
 }
 
-module.exports = { connectToMongoDB, client };
+// Definir o schema para a coleção "ontimes"
+const ontimesSchema = new mongoose.Schema({
+  nome: String,
+  cargo: String,
+  areaInteresse: String,
+});
+
+// Criar um modelo com base no schema
+const Ontime = mongoose.model('Ontime', ontimesSchema);
+
+module.exports = { connectToMongoDB, client, ObjectId, Ontime };
